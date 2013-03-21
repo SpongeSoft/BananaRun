7ab4ca341d816916c532acd9ced78b6ae1635a96
satanete <alvaro.sperez@gmail.com>
2013-03-21 12:26:32 +0100

    -Texto sobre radiobuttons -Radiobuttons ahora hacen algo

--- src/com/spongesoft/dietapp/SessionSettingsActivity.java
+++ src/com/spongesoft/dietapp/SessionSettingsActivity.java
@@ -2,19 +2,47 @@ package com.spongesoft.dietapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.NumberPicker;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class SessionSettingsActivity extends Activity{
-
+	
 	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
         setContentView(R.layout.session_settings);
         
         //Number picker parameters 
-        NumberPicker np = (NumberPicker) findViewById(R.id.limit_picker);
+        final NumberPicker np = (NumberPicker) findViewById(R.id.limit_picker);
         np.setMaxValue(300);
         np.setMinValue(1);
+
+
+        final RadioButton r1 = (RadioButton) findViewById(R.id.time_limit);
+        final RadioButton r2 = (RadioButton) findViewById(R.id.distance_limit);
+        final RadioButton r3 = (RadioButton) findViewById(R.id.free_running);
+        
+        r1.setOnClickListener(new View.OnClickListener(){
+        	public void onClick(View v) {
+        			np.setVisibility(View.VISIBLE);
+        	}	
+        });
+        
+        r2.setOnClickListener(new View.OnClickListener(){
+        	public void onClick(View v) {
+            	   np.setVisibility(View.VISIBLE);
+        	}
+        });
+        
+        r3.setOnClickListener(new View.OnClickListener(){
+        	public void onClick(View v) {
+            	   np.setVisibility(View.INVISIBLE);
+        	}
+        });
+        
+        
         
     }
 	
