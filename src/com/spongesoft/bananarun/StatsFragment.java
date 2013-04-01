3b942c0b9c14bdd6c4fff884016f7a49974d5734
kadaki01 <juanljr2@gmail.com>
2013-04-01 18:25:37 +0200

    Added stat branch Created layout for new fragment

---
+++ src/com/spongesoft/bananarun/StatsFragment.java
@@ -0,0 +1,46 @@
+package com.spongesoft.bananarun;
+
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.Animation.AnimationListener;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.spongesoft.dietapp.R;
+
+public class StatsFragment extends Fragment {
+	/**
+	 * The fragment argument representing the section number for this fragment.
+	 */
+	public static final String ARG_SECTION_NUMBER = "section_number";
+
+	/**
+	 * Buttons and whatnot
+	 */
+
+	/**
+	 * Methods
+	 */
+	public StatsFragment() {
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		// Create a new TextView and set its text to the fragment's section
+		// number argument value.
+		final View HomeView = inflater.inflate(R.layout.stats, container,
+				false);
+
+		return HomeView;
+	}
+
+}
