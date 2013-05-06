f0a1fd47d02a9328f770890b68223a894a79b621
Alejandro <zen@itram.es>
2013-05-06 09:39:17 +0200

    Fix package

--- src/com/spongesoft/bananarun/ListGraphsActivity.java
+++ src/com/spongesoft/bananarun/ListGraphsActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 
 import com.spongesoft.bananarun.LineChart;
 import com.spongesoft.bananarun.MainActivity;
-import com.spongesoft.dietapp.R;
+import com.spongesoft.bananarun.R;
 import android.view.View;
 import android.view.View.OnClickListener;
 
