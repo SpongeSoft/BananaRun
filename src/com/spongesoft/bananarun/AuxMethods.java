ad90645956e28bdaed7d4bbd3eaf55d64cb83ada
navlin07 <koke1701@gmail.com>
2013-05-08 00:50:46 +0200

    aux coments

--- src/com/spongesoft/bananarun/AuxMethods.java
+++ src/com/spongesoft/bananarun/AuxMethods.java
@@ -5,13 +5,27 @@ import java.text.DecimalFormatSymbols;
 import java.util.Locale;
 
 import android.content.SharedPreferences;
+import android.graphics.drawable.Drawable;
 
 public class AuxMethods {
+	
+	/**
+	 * Constructor
+	 */
 
 	SharedPreferences preferences;
 	public AuxMethods(SharedPreferences preferences){
 		this.preferences = preferences;
 	}
+	
+	
+	/**
+	 * Get Distance with unit
+	 * 
+	 * @param Distance to concatenate
+	 * 
+	 * @return String value of distance + unit  
+	 */
 	
 	public String getDistance(double d) {
 		
@@ -33,7 +47,14 @@ public class AuxMethods {
 		
         return dist + Unit;
 	}
-
+	
+	/**
+	 * Get height and weight from preferences menu
+	 * 
+	 *  
+	 * @return Array of integers with both values
+	 */
+	
 	public int[] getHeightAndWeight(){
 		
 		int[] abundle = new int[2];
@@ -44,11 +65,24 @@ public class AuxMethods {
 		return abundle;
 	}
 	
+	/**
+	 * Get the number given with only two decimals
+	 * 
+	 * @param Number to convert
+	 *      
+	 * @return   The number with only two decimals   
+	 */
+	
 	public double stripDecimals(double value) {
 		DecimalFormat twoDForm = new DecimalFormat("#.##",new DecimalFormatSymbols(Locale.US));
 		return Double.valueOf(twoDForm.format(value));
 	}
 	
+	/**
+	 * Get type of measure
+	 * 
+	 *@return  String with type of measure        
+	 */
 	public String getDistanceUnits() {
 		String distUnit = preferences.getString("prefUnitSystem", "1");
 		if(distUnit.equals("2")){
